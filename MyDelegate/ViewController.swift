1376e5810b053756e56a62aaf43ce8d9d78cc569
Leslie <leslie.appl@gmail.com>
2020-12-21 16:46:16 +0800

    Initial Commit

---
+++ MyDelegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDelegate
+//
+//  Created by leslie on 12/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
